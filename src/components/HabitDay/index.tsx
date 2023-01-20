879cdad74954e763ff6b1297cbb1cb4800045817
Jeferson Mesquita <jeferson.mesquita@imobpower.com>
2023-01-20 08:43:58 -0300

    Aula 04 - Ajustes de Layout

--- src/components/HabitDay/index.tsx
+++ src/components/HabitDay/index.tsx
@@ -16,7 +16,7 @@ export function HabitDay({ amount, completed }: HabitDayProps) {
   return (
     <Popover.Root>
       <Popover.Trigger
-        className={clsx('w-10 h10 border-2 rounded-lg', {
+        className={clsx('w-10 h10 border-2 rounded-lg hover:border-zinc-400 ', {
           'bg-violet-900 border-violet-700': completedPercentage >= 80,
           'bg-violet-800 border-violet-600':
             completedPercentage >= 60 && completedPercentage < 80,
@@ -47,13 +47,13 @@ export function HabitDay({ amount, completed }: HabitDayProps) {
 
           <div className="mt-6 flex flex-cl gap-3">
             <Checkbox.Root className="flex items-center gap-3 group">
-              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500">
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-700 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
                 <Checkbox.Indicator>
-                  <Check size={20} className='text-white' weight='bold' />
+                  <Check size={20} className="text-white" weight="bold" />
                 </Checkbox.Indicator>
               </div>
 
-              <span className="font-semibold text-xl text-white leading-tight">
+              <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-500">
                 Beber 2L de Água
               </span>
             </Checkbox.Root>
